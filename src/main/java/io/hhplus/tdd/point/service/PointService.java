006b8e161acfe398f3cbe9667476d0ce242cf3f9
wookyoungK <pd1125@naver.com>
2025-03-28 07:03:55 +0900

    feat : 동시성 테스트를 위한 동시성 제어 추가

--- src/main/java/io/hhplus/tdd/point/service/PointService.java
+++ src/main/java/io/hhplus/tdd/point/service/PointService.java
@@ -8,11 +8,13 @@ import io.hhplus.tdd.exceptions.PointOverException;
 import io.hhplus.tdd.point.PointHistory;
 import io.hhplus.tdd.point.TransactionType;
 import io.hhplus.tdd.point.UserPoint;
+import io.hhplus.tdd.point.handler.LockManager;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.concurrent.locks.ReentrantLock;
 
 @Service
 @RequiredArgsConstructor
@@ -21,7 +23,7 @@ public class PointService implements IPointService {
 
     private final PointHistoryTable pointHistoryTable;
     private final UserPointTable userPointTable;
-
+    private final LockManager lockManager;
     /**
      * 특정 유저의 포인트를 조회하는 기능
      */
@@ -43,33 +45,49 @@ public class PointService implements IPointService {
      * 특정 유저의 포인트를 충전하는 기능
      */
     @Override
-    public UserPoint charge(Long id, Long  amount) {
+    public UserPoint charge(Long id, Long amount) {
         if (amount < 0) {
             throw new PointOverException(amount);
         }
-        UserPoint currentPoint = userPointTable.selectById(id);
-        long newAmount = currentPoint.point() + amount;
-        if (newAmount > 1000000L) {
-            throw new PointMaxException(newAmount);
-        }
-        UserPoint updatedPoint = userPointTable.insertOrUpdate(id, newAmount);
-        pointHistoryTable.insert(id, amount, TransactionType.CHARGE, System.currentTimeMillis());
-        return updatedPoint;
-    }
+        ReentrantLock lock = lockManager.getLock(id);
+        lock.lock();
+        try {
+            UserPoint currentPoint = userPointTable.selectById(id);
 
+            long newAmount = currentPoint.point() + amount;
+            if (newAmount > 1000000L) {
+                throw new PointMaxException(newAmount);
+            }
+
+            UserPoint updatedPoint = userPointTable.insertOrUpdate(id, newAmount);
+            pointHistoryTable.insert(id, amount, TransactionType.CHARGE, System.currentTimeMillis());
+
+            return updatedPoint;
+        } finally {
+            lock.unlock();
+        }
+    }
     /**
      * 특정 유저의 포인트를 사용하는 기능
      */
     @Override
     public UserPoint use(Long id, Long amount) {
+        ReentrantLock lock = lockManager.getLock(id);
+        lock.lock();
+        try {
+            UserPoint currentPoint = userPointTable.selectById(id);
 
-        UserPoint currentPoint = userPointTable.selectById(id);
-        if (currentPoint.point() < amount) {
-            throw new PointNotException(amount);
+            if (currentPoint.point() < amount) {
+                throw new PointNotException(amount);
+            }
+
+            long newAmount = currentPoint.point() - amount;
+            UserPoint updatedPoint = userPointTable.insertOrUpdate(id, newAmount);
+            pointHistoryTable.insert(id, amount, TransactionType.USE, System.currentTimeMillis());
+
+            return updatedPoint;
+        } finally {
+            lock.unlock();
         }
-        long newAmount = currentPoint.point() - amount;
-        UserPoint updatedPoint = userPointTable.insertOrUpdate(id, newAmount);
-        pointHistoryTable.insert(id, amount, TransactionType.USE, System.currentTimeMillis());
-        return updatedPoint;
     }
 }
